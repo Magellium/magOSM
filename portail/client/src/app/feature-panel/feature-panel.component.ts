0e729fa6114c54c535fee9cbc0b71f03e87bd571
Axel DUMONT <axel.dumont@magellium.fr>
2019-04-25 10:48:08 +0200

    New feature panel

--- portail/client/src/app/feature-panel/feature-panel.component.ts
+++ portail/client/src/app/feature-panel/feature-panel.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, ViewChild } from '@angular/core';
 import { Layer } from '../model/Layer';
 import { MapService } from '../service/map.service';
+import { FeatureMainInfoComponent } from 'app/feature-panel-components/feature-main-info/feature-main-info.component';
 
 declare var $: any;
 declare var config: any;
-
+declare var _paq: any;
 
 @Component({
   selector: 'app-feature-panel',
@@ -16,12 +17,13 @@ export class FeaturePanelComponent implements OnInit {
   @Output()
   layervariables = config.LAYERS;
 
-  //Pour utiliser la variable config dans le html
+  //To use the variable config in the html
   config = config;
 
   @Input() public selectedFeatures: any;
   @Input() public selectedLayer: Layer;
 
+  @ViewChild(FeatureMainInfoComponent) featureMainInfoComponent: FeatureMainInfoComponent;
 
   public selectedFeature: any;
   private panelOpen = true;
@@ -32,7 +34,6 @@ export class FeaturePanelComponent implements OnInit {
   constructor(private mapService: MapService) { }
 
   ngOnInit() {
-    console.log(this.selectedFeatures);
     var self=this;
     $("#featurepanel-close").click(function(e) {
       e.preventDefault();
@@ -42,11 +43,41 @@ export class FeaturePanelComponent implements OnInit {
 
     this.selectedFeature = this.selectedFeatures[0];
     this.processAttributes();
+    this.updateSelectedLayer(this.selectedFeature);
+  }
+
+  onSelect(feature) {
+    this.selectedFeature =feature;
+    this.processAttributes();
+    this.mapService.cleanSelection();
+    this.mapService.addToSelection(feature);
+    this.updateSelectedLayer(this.selectedFeature);
+  }
+
+  ngOnChanges(...args: any[]) {//si l'@Input() selectedFeature change, cette fonction est lancée
+    console.log('FeaturePanel : ngOnChanges')
+    if (!this.panelOpen) { //si le panel est fermé, on l'ouvre
+      console.log('onChange fired');
+      console.log('changing', args);
+      //$("#fpanel").show();
+      this.panelOpen = true;
+      if(!$("#featurepanel-wrapper").hasClass("active")) {
+        $("#featurepanel-wrapper").toggleClass("active");
+      }
+    }
+    this.selectedFeature = this.selectedFeatures[0];
+    this.processAttributes();
+    this.updateSelectedLayer(this.selectedFeature);
+  }
+
+  ngAfterViewInit() {
   }
   
+  //Find the attributes and separate them in to array : keys and technicals_keys ("osm_...")
   processAttributes() {
     let geomType = this.selectedFeature.getGeometry().getType();
     let clefs = this.selectedFeature.getKeys();
+    //Determine the osm_type
     if (geomType == "Point") {
       this.osm_type = "node";
     }
@@ -65,7 +96,6 @@ export class FeaturePanelComponent implements OnInit {
     this.technical_keys=[];
     this.keys=[];
     for (let key in clefs) {
-      console.log(clefs[key]);
       if ('boundedBy' != clefs[key] && 'way' != clefs[key] && 'st_union' != clefs[key] && 'the_geom' != clefs[key]) {
         if ('osm_' == clefs[key].substr(0, 4)) {
           if('osm_uid' != clefs[key] && 'osm_user' != clefs[key] && 'osm_changeset' != clefs[key] && 'osm_original_geom' != clefs[key]){
@@ -79,32 +109,57 @@ export class FeaturePanelComponent implements OnInit {
 
     }
   }
-  onSelect(feature) {
-    this.selectedFeature =feature;
-    this.processAttributes();
-     this.mapService.cleanSelection();
-      this.mapService.addToSelection(feature);
+   
+  //Keys are written with "-" instead of a ":" in the DB
+  public formatKey(key :string) : string{
+    return key.replace(/-/g,':');
   }
 
-  formatKey(k){
-    return k.replace(/-/g,':');
-  }
-  ngOnChanges(...args: any[]) {//si l'@Input() selectedFeature change, cette fonction est lancée
-    console.log('FeaturePanel : ngOnChanges')
-    if (!this.panelOpen) { //si le panel est fermé, on l'ouvre
-      console.log('onChange fired');
-      console.log('changing', args);
-      //$("#fpanel").show();
-      this.panelOpen = true;
-      if(!$("#featurepanel-wrapper").hasClass("active")) {
-        $("#featurepanel-wrapper").toggleClass("active");
+  //For the list of nearby features, this function is called for each features on the dropdown-menu
+  public getLabelToDisplay(feature) :string{
+    var layer = this.mapService.getLayerOfOneFeature(feature);
+    var featureImportantTagsList = this.featureMainInfoComponent.getKeyAndLabelForAllLevelsOfImportance(feature,layer); 
+
+    if (!featureImportantTagsList.isEmpty()){ //if the list is not null, return the high important key
+    var highImportanceList = featureImportantTagsList.list.filter(x => x.importance=== "high");
+      if (highImportanceList.length >0){// If highImportanceList is not empty
+        var highImportanceTag = highImportanceList[0].tagList;
+        if (!highImportanceTag.isEmpty()){
+        return feature.get(highImportanceList[0].tagList.list[0].key);
+        }
       }
     }
-    this.selectedFeature = this.selectedFeatures[0];
-    this.processAttributes();
+    //if the list is null (ie there is nothing to display) or there is no high important key to display, return the name of the layer and the osm_id.
+    return layer.nom_court+' : '+Math.abs(feature.get('osm_id'));
+  }
+ 
+  //the selectedLayer changes when the user clicks on a feature which belongs to on another layer
+  private updateSelectedLayer(feature):void{
+    this.selectedLayer= this.mapService.getLayerOfOneFeature(feature);
   }
 
-  ngAfterViewInit() {
+  // For the display on the table : if the value is NonRelevant, the logos won't appear on the second row.
+  // The function reads the config
+  public isValueIrrelevant(tag : string) : boolean{
+    let isValueIrrelevant = false;
+    config.OSM_KEYS_WITH_IRRELEVANT_VALUE.forEach(function(n){
+      if (tag.includes(n.osm_key)){
+        isValueIrrelevant=true;
+      }
+    })
+    return isValueIrrelevant;
+  }
+    
+  //Matomo Events
+  tableSelected(table){
+    if (!(document.getElementById("collapse"+table).classList.contains('show'))){
+      _paq.push(['trackEvent', 'table_selected', table]);
+    }
+  }
+  dropdownMenuSelected(){
+    if (!(document.getElementById("features_nearby_list").classList.contains('show'))){
+      _paq.push(['trackEvent', 'features_nearby_list_selected']);
+    }
   }
 
 }
