232015d9df292cf2e4b45fd0eea2d000323f60ae
audour <xhemp@mykolab.com>
2019-06-25 15:38:56 +0200

    service-webapp-magosm: initial comit

---
+++ services-webapp-magosm/src/main/java/fr/magellium/magosm/MagosmApplication.java
@@ -0,0 +1,13 @@
+package fr.magellium.magosm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagosmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagosmApplication.class, args);
+	}
+
+}
