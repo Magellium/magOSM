be830b016f22dd6fb9c13d34b2062dd88cd70e70
axdumo <axelgc974@gmail.com>
2019-07-15 13:44:47 +0200

    5432 --> 5433 + modif ChangeType

--- services-webapp-magosm/src/main/java/com/magellium/magosm/model/ChangeType.java
+++ services-webapp-magosm/src/main/java/com/magellium/magosm/model/ChangeType.java
@@ -10,7 +10,7 @@ import javax.persistence.Table;
 
 @Entity
 @Table(name = "changes_types", schema = Parameters.MAGOSM_DICTIONNARY_SCHEMA)
-@NamedQuery(name = "ChangeType.findAll", query = "SELECT t FROM ChangeType t")
+@NamedQuery(name = "ChangeType.findAll", query = "SELECT t FROM ChangeType t ORDER BY name")
 public class ChangeType {
 	
 	@Id
@@ -20,6 +20,15 @@ public class ChangeType {
 	
 	@Column(name = "name")
 	private String name;
+	
+	@Column(name = "short_name")
+	private String short_name;
+	
+	@Column(name = "color")
+	private String color;
+	
+	@Column(name = "label")
+	private String label;
 
 	public Integer getId() {
 		return id;
@@ -28,6 +37,7 @@ public class ChangeType {
 	public void setId(Integer id) {
 		this.id = id;
 	}
+	
 
 	public String getName() {
 		return name;
@@ -36,5 +46,31 @@ public class ChangeType {
 	public void setName(String name) {
 		this.name = name;
 	}
+
+	public String getShort_name() {
+		return short_name;
+	}
+
+	public void setShort_name(String short_name) {
+		this.short_name = short_name;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public String getLabel() {
+		return label;
+	}
+
+	public void setLabel(String label) {
+		this.label = label;
+	}
+	
+	
 
 }
