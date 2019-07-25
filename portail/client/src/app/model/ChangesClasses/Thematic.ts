86d5ad1f8b310a1d377852011d107af1f4145dbf
axdumo <axelgc974@gmail.com>
2019-07-25 16:25:10 +0200

    new class : ThematicCategory

--- portail/client/src/app/model/ChangesClasses/Thematic.ts
+++ portail/client/src/app/model/ChangesClasses/Thematic.ts
@@ -1,12 +1,16 @@
+import { ThematicCategory } from "./ThematicCategory";
+
 export class Thematic {
     name:string;
     id:number;
     changesRequest:string;
+    category:ThematicCategory;
   
-    constructor(name : string ,id : number, changesRequest : string){
+    constructor(name : string ,id : number, changesRequest : string, category:ThematicCategory){
       this.name= name;
       this.id = id;
       this.changesRequest = changesRequest;
+      this.category = category;
     }
 
     public getName() : string {
