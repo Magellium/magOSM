da130a76b69c66ac2deadc3be18e572f5bac80f6
axdumo <axelgc974@gmail.com>
2019-07-25 16:25:43 +0200

    help component update

--- portail/client/src/app/help/help.component.ts
+++ portail/client/src/app/help/help.component.ts
@@ -1,15 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+
+declare var $;
 
 @Component({
   selector: 'app-help',
   templateUrl: './help.component.html',
   styleUrls: ['./help.component.css']
 })
-export class HelpComponent implements OnInit {
+export class HelpComponent implements AfterViewInit {
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(router: Router) {
+    /* Lorsque le composant est chargé depuis une URL contenant une ancre :
+    * - l'élément du DOM avec l'id correspondant à l'ancre n'est pas encore chargé
+    * - on attend donc la fin de la navigation pour scroller jusqu'à l'ancre
+    */
+    router.events.subscribe(s => {
+      if (s instanceof NavigationEnd) {
+        const tree = router.parseUrl(router.url);
+        if (tree.fragment) {
+          const element = document.querySelector("#" + tree.fragment);
+          if (element) { element.scrollIntoView(true); }
+        }
+      }
+    });
   }
+
+  ngAfterViewInit() {
+    /* on ne déclare le scrollspy qu'une fois que le template est initialisé */
+    $(document).ready(() => {
+      $('body').scrollspy({target: "#navbar-example3", offset: 50});   
+    });
+
+
+    /* rustine pour que la navbar de Bootstrap cohabitent avec Angular et sa gestion des href :
+    * - Bootstrap a besoin d'une href correspondant à une ancre (ex: href="#umap")
+    * - Angular a besoin d'un chemin complet pour suivre un lien (ex: aide.html#umap)
+    * ==> on conserve le format qui va bien à Bootstrapau au
+    * ==> clic sur un élément du menu, on stop la navigation native d'Angular pour scroller vers l'ancre
+    */
+    $(".nav-link").click(function(event) {
+      event.preventDefault();
+      let fragment = event.target.hash;
+      const element = document.querySelector(fragment);
+      if (element) { element.scrollIntoView(true); }
+        });
+
+  }
+
 
 }
