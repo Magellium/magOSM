579848417edf552e30c266760582628428639c65
axdumo <axelgc974@gmail.com>
2019-08-01 15:41:45 +0200

    slider service

--- portail/client/src/app/service/slider.service.ts
+++ portail/client/src/app/service/slider.service.ts
@@ -1,13 +1,62 @@
 import { Injectable } from '@angular/core';
-import { LabelType } from 'ng5-slider';
+import { LabelType, Options } from 'ng5-slider';
 import { Change } from 'app/model/ChangesClasses/Change';
+import { ChangeType } from 'app/model/ChangesClasses/ChangeType';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SliderService {
 
+  public options : Options;
+  public stepsArray;
+  public ticksArray;
+  public minValue;
+  public maxValue;
+
+  public getMinValue(){
+    return this.minValue;
+  }
+  public getMaxValue(){
+    return this.maxValue;
+  }
+  public getOptions(){
+    return this.options;
+  }
   constructor() { }
+
+  public initSlider(beginDate, endDate, featureChangesList, changeType){
+    this.stepsArray = this.getStepsArray(beginDate, endDate);
+    this.ticksArray = this.getTicksArray(featureChangesList, this.stepsArray);
+    this.minValue = beginDate.getTime();
+    this.maxValue = endDate.getTime();
+    this.setOptions(this.stepsArray, this.ticksArray, changeType);
+
+  }
+  public setOptions(stepsArray, ticksArray, changeType){
+    var dateOptions = { month: 'numeric', day: 'numeric' };
+    this.options = {
+      stepsArray : stepsArray.map((date : number) => {return {value : date, legend : "<b>"+new Date(date).toLocaleDateString('fr-FR', dateOptions)+"</b>"}}),
+      translate: this.translate,
+      noSwitching: true,
+      showTicks: true,
+      ticksArray : ticksArray,
+      showSelectionBar:false,
+      getTickColor: (value: number): string => {
+        if (value == 6) {
+          return 'blue';
+        }
+        if (value == 2){
+          return 'red'
+        }
+        return '#2AE02A';
+      },
+      // getSelectionBarColor: (minValue : number, maxValue : number): string => {
+      //   console.log(changeType);
+      //   return this.getRGBA(changeType);
+      // }
+    };
+  }
 
   public getStepsArray(beginDate :Date, endDate :Date) : any[]{
     if (endDate.getTime() < beginDate.getTime()){
@@ -25,7 +74,8 @@ export class SliderService {
   }
 
   public translate(value: number, label: LabelType) : string{
-    return new Date(value).toLocaleDateString('fr-FR');
+    var dateOptions = { month: 'numeric', day: 'numeric' };
+    return new Date(value).toLocaleDateString('fr-FR', dateOptions);
   }
 
   public getTicksArray(changesList : Array<Change>, stepsArray : any[]) : Array<number>{
@@ -38,5 +88,9 @@ export class SliderService {
       ticksArray.push(stepsArray.indexOf(timestamp.getTime()));
     });
     return ticksArray;
+  }
+
+  public getRGBA(changeType : ChangeType){
+    return "rgba("+changeType.color.R+","+changeType.color.G+","+changeType.color.B+")"
   }
 }
