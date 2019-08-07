917de6395c14f07f7755470d77e968e718ddcf63
axdumo <axelgc974@gmail.com>
2019-08-07 11:06:53 +0200

    baseUrl from WebApp

--- portail/client/src/app/service/api-request.service.ts
+++ portail/client/src/app/service/api-request.service.ts
@@ -3,14 +3,17 @@ import { throwError as observableThrowError, Observable} from 'rxjs';
 import { Http } from '@angular/http';
 import { catchError } from 'rxjs/operators';
 import { Thematic } from 'app/model/ChangesClasses/Thematic';
+import { resolve } from 'dns';
+import { ConfigService } from './config.service';
 
 declare var config : any;
+
 @Injectable({
   providedIn: 'root'
 })
 export class ApiRequestService {
 
-  private baseUrl = config.WEBAPP_BASE_URL;
+  private baseUrl : string ;
   private thematicsSuffix =  'thematics';
   private changesSuffix = 'changesrequest';
   private changeTypesSuffix = 'change_types';
@@ -18,10 +21,22 @@ export class ApiRequestService {
   public beginDate : Date;
   public endDate : Date;
   public thematic : Thematic;
+  public obs = new Observable<any>(res => {
+    res.next(config);
+  })
 
-  constructor(public http: Http) { }
+  constructor(
+    public http: Http,
+    public configService : ConfigService) 
+    { 
+      this.configService.getConfig().subscribe(config => {
+        this.baseUrl = config.WEBAPP_BASE_URL;
+      }) 
+    }
 
   public searchThematics(): Observable<any> {
+    console.log(this.baseUrl);
+    console.log(config);
     return this.http.get(this.baseUrl + this.thematicsSuffix)
   };
 
