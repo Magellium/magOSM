162f622052ea568f635ac5544d8128c415fb943e
audour <xhemp@mykolab.com>
2020-03-19 19:57:46 +0100

    Add a staging environment

---
+++ portail/client/src/environments/environment.staging.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  staging: true,
+  geoserver_baseurl: "http://172.30.0.52/geoserver",
+  services_baseurl: "http://localhost/services-webapp-magosm/"
+};
